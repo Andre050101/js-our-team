f5d55d68e48d2894d9570afb6f6b26267c0a315d
Andre050101 <agiancristiano90@gmail.com>
2024-11-13 17:01:42 +0100

    Card added with js

--- script/script.js
+++ script/script.js
@@ -37,6 +37,37 @@ const teamMembers = [
     }
 ];
 
+/*CARD HTML:
 
+    
+
+*/
+
+/*Contenitore card:*/
+const container = document.getElementById("card-container");
+
+/*Iterazione sull'array per creare card per ciascun elemento:*/
+teamMembers.forEach(member => {
+    //Creazione elemento card
+    const card = document.createElement('div');
+    card.classList.add('col-md-4', 'mb-4');
+
+    //Contenuto card:
+    card.innerHTML = `
+    <div class="card-custom">
+        <div class="width30per">
+            <img src="${member.img}" alt="${member.name}">
+        </div>
+        <div class="width70per ps-3">
+            <h2>${member.name}</h2>
+            <p>${member.role}</p>
+            <a href="mailto:${member.email}">${member.email}</a>
+        </div>
+    </div>
+    `;
+
+    //Aggiungo card a container:
+    container.appendChild(card);
+});
 
 
